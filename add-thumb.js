520f4ab0f184105f897f7c96fe7264c59d8f531b
Aditya Sethi <techyguyaditya@gmail.com>
2023-11-08 01:45:39 +0530

    CSS fixes and removing logs for production usage

--- add-thumb.js
+++ add-thumb.js
@@ -2,13 +2,10 @@ async function add_thumb() {
     var mess = await browser.runtime.sendMessage({command: "getYoutubeId"});
     if( mess == "" ){ return; }
 
-    console.log(mess);
-
     var link = mess.headers["content-base"][0];
     var regexp = /https:\/\/www.youtube.com\/watch\?v=/;
     if( !regexp.test(link) ) { return; }
     var video_id = link.replace(regexp,"");
-    console.log(video_id);
     var thumb_url = `https://i.ytimg.com/vi/${video_id}/maxresdefault.jpg`;
 
     var a = document.createElement("a");
